13eb9e7d5167e1f299ec71aa609a12f53bba77c1
伊藤健太 <jiantaiyiteng1@gmail.com>
2025-02-26 15:00:30 +0900

    １から簡単なTodoアプリを作ってReactの１歩を踏み出してみよう

--- enviroment_building/src/App.tsx
+++ enviroment_building/src/App.tsx
@@ -3,6 +3,8 @@ import './App.css';
 import TodoList from './TodoList';
 
 function App() {
+  // コンポーネントに記述することで、直接コードを記述するよりも
+  // 運用メンテナンスしやすくなる
   return (
     <div className="App">
       <TodoList />
